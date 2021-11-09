6292ccd92a40f64392a98c8badabe1da2ab5fe29
Melquesedeque O da Silva <123melque@gmail.com>
2021-11-09 01:22:02 -0300

    jogador

--- app/Http/Controllers/JogadoresController.php
+++ app/Http/Controllers/JogadoresController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Jogador;
+use App\Models\Posicao;
+use App\Models\Clube;
+use Illuminate\Support\Facades\DB;
 
 class JogadoresController extends Controller
 {
@@ -13,7 +17,19 @@ class JogadoresController extends Controller
      */
     public function index()
     {
-        //
+        $jogador = new Jogador();
+		$jogadores = DB::table("jogador AS j")->join("posicao AS p","j.posicao", "=","p.id")->join("clube AS c","j.clube", "=","c.id")->select("j.nome_jogador","j.data_nascimento","p.pos AS posicao", "c.nome_clube AS clube", "j.ehColecao", "j.id")->get();
+		$posicoes = Posicao::All();
+		$clubes = Clube::All();
+		return view(
+			"futebol-figurinha-2021.jogador.jogador",
+			[
+				"jogador" => $jogador,
+				"jogadores" => $jogadores,
+				"posicoes" => $posicoes,
+				"clubes" => $clubes
+			]
+		);
     }
 
     /**
@@ -34,7 +50,27 @@ class JogadoresController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if($request->get("id")== ""){
+			$jogador = new Jogador();
+			$acao = "salvo";
+		}else{
+			$jogador = Jogador::Find($request->get("id"));
+			$acao = "editado";
+		}
+		$jogador->nome_jogador = $request->get("nome_jogador");
+		$jogador->data_nascimento  = $request->get("data_nascimento");
+		$jogador->posicao = $request->get("posicao");
+		$jogador->clube = $request->get("clube");
+		$col = $request->get("ehColecao");
+		
+		if($col == "" || $col == "N"){
+			$col = "N";
+		}
+		$jogador->ehColecao  = $col;
+		$jogador->save();
+        $request->Session()->flash("acao", $acao);
+		return redirect("/jogador");
+	
     }
 
     /**
@@ -56,7 +92,20 @@ class JogadoresController extends Controller
      */
     public function edit($id)
     {
-        //
+        new Jogador();
+		$jogador = Jogador::Find($id);
+		$jogadores = DB::table("jogador AS j")->join("posicao AS p","j.posicao", "=","p.id")->join("clube AS c","j.clube", "=","c.id")->select("j.nome_jogador","j.data_nascimento","p.pos AS posicao", "c.nome_clube AS clube", "j.ehColecao", "j.id")->get();
+		$posicoes = Posicao::All();
+		$clubes = Clube::All();
+		return view(
+			"futebol-figurinha-2021.jogador.jogador",
+			[
+				"jogador" => $jogador,
+				"jogadores" => $jogadores,
+				"posicoes" => $posicoes,
+				"clubes" => $clubes
+			]
+		);
     }
 
     /**
@@ -77,8 +126,10 @@ class JogadoresController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id,Request $request)
     {
-        //
+        Jogador::Destroy($id);
+        $request->Session()->flash("acao", "excluido");
+		return redirect("/jogador");
     }
 }
--- resources/views/futebol-figurinha-2021/jogador/jogador.blade.php
+++ resources/views/futebol-figurinha-2021/jogador/jogador.blade.php
@@ -4,20 +4,36 @@
 	<div class = "conteiner col-2">
 		<form class = "row-2"action = "/jogador" method = "post">
 			<div>
-				<label for  = "campoNome">Nome do clube</label>
+				<label for  = "campoNome">Nome do jogador</label>
 				<input type = "text" id = "campoNome" name = "nome_jogador" value = "{{$jogador->nome_jogador}}"/>
 			</div>
 			<div>
-				<label for  = "campoData">Nome do clube</label>
+				<label for  = "campoData">Data de nascimento</label>
 				<input type = "date" id = "campoData" name = "data_nascimento" value = "{{$jogador->data_nascimento}}"/>
 			</div>
 			<div>
-				<label for  = "selecaoClube">Nome do clube</label>
-				<input type = "text" id = "selecaoClube" name = "clube" value = "{{$jogador->clube}}"/>
+				<label>Clube</label>
+				<select id = "selecaoClube" name = "clube" >
+					@foreach($clubes as $clube)	
+						@if($jogador->clube == $clube->id)
+							<option value = "{{$clube->id}}" selected = "selected">{{$clube->nome_clube}}</option>
+						@else
+							<option value = "{{$clube->id}}" >{{$clube->nome_clube}}</option>
+						@endif
+					@endforeach
+				</select>
 			</div>
 			<div>
-				<label for  = "selecaoPosicao">Nome do clube</label>
-				<input type = "text" id = "selecaoPosicao" name = "posicao" value = "{{$jogador->posicao}}"/>
+				<label>Posicao</label>
+				<select id = "selecaoPosicao" name = "posicao" >
+					@foreach($posicoes as $posicao)	
+						@if($jogador->posicao == $posicao->id)
+							<option value = "{{$posicao->id}}" selected = "selected">{{$posicao->pos}}</option>
+						@else
+							<option value = "{{$posicao->id}}" >{{$posicao->pos}}</option>
+						@endif
+					@endforeach
+				</select>
 			</div>
 			<div>
 				<input type = "submit" value  = "Salvar"/>
@@ -41,7 +57,12 @@
 		<tbody>
 			@foreach($jogadores as $jogador)
 			<tr>
-				<td>{{$jogador->nome_clube}}</td>
+				
+				<td>{{$jogador->nome_jogador}}</td>
+				<td>{{$jogador->data_nascimento}}</td>
+				<td>{{$jogador->posicao}}</td>
+				<td>{{$jogador->clube}}</td>
+				<td>{{$jogador->ehColecao}}</td>
 				<td><a href = "/jogador/{{$jogador->id}}/edit">Editar</a><td/>
 				<td>
 					<form method = "POST" action = "/jogador/{{$jogador->id}}">
